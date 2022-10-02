179a9a5624ad8a2c12c13ffd99d6a4188a336d85
mourice-oduor <otienomaurice364@gmail.com>
2022-10-02 19:40:11 +0300

    Update test spec

--- 01-Test/test/add.spec.js
+++ 01-Test/test/add.spec.js
@@ -1,12 +1,15 @@
 // Import add function from add.js
 const add = require('../add')
+const expect = require('expect')
+const assert = require('assert')
 
 // Test suite
-// describe('add', () => {
+// describe('add function', () => {
 //     // Test spec (unit test)
 //     it('adds two numbers', () => {
 //         // Test case
-//         expect(add(1, 2)).toBe(3)
+//         // expect(add(1, 2)).toBe(3)
+//         assert.equal(add(1, 2), 3)
 //     })
 // })
 
@@ -15,6 +18,7 @@ describe('The add function', () => {
         const actual = add(1, 3)
         const expected = 4
 
-        expect(actual).toBe(expected)
+        // expect(actual).toBe(expected)
+        assert.equal(actual, expected)
     })
 })
