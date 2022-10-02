c0a11eebe20fb20f6cda44d93ca9e4e3cb03b722
mourice-oduor <otienomaurice364@gmail.com>
2022-10-02 18:55:33 +0300

    Update function spec

--- 01-Test/test/add.spec.js
+++ 01-Test/test/add.spec.js
@@ -1,4 +1,14 @@
+// Import add function from add.js
 const add = require('../add')
+
+// Test suite
+// describe('add', () => {
+//     // Test spec (unit test)
+//     it('adds two numbers', () => {
+//         // Test case
+//         expect(add(1, 2)).toBe(3)
+//     })
+// })
 
 describe('The add function', () => {
     it('adds two numbers', () => {
