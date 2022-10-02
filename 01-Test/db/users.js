70432254b05eb47d658f08eff8c9b4f8fe872950
mourice-oduor <otienomaurice364@gmail.com>
2022-10-02 20:27:34 +0300

    users data

---
+++ 01-Test/db/users.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    id: 1,
+    age: 29,
+    name: "morys0",
+    email: "morys0@gmail.com",
+    registered_on: "2022-01-01T00:00:00.000Z",
+  },
+  {
+    id: 2,
+    age: 29,
+    name: "morys1",
+    email: "morys1@gmail.com",
+    registered_on: "2022-01-01T00:00:00.000Z",
+  },
+  {
+    id: 3,
+    age: 29,
+    name: "morys2",
+    email: "morys1@gmail.com",
+    registered_on: "2022-01-01T00:00:00.000Z",
+  },
+];
