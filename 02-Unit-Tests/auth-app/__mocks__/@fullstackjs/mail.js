469f358ec528e253836aad9bca8b029301506e40
mourice-oduor <otienomaurice364@gmail.com>
2022-10-13 17:33:07 +0300

    mocks

---
+++ 02-Unit-Tests/auth-app/__mocks__/@fullstackjs/mail.js
@@ -0,0 +1,15 @@
+module.exports = class Mail {
+    to() {
+        return this
+    }
+    subject() {
+        return this
+    }
+    data() {
+        return this
+    }
+    send() {
+        return this
+    }
+}
+
