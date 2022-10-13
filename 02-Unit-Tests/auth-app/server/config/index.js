c7512a34ef58df3832c844f9aa884f0081e215db
mourice-oduor <otienomaurice364@gmail.com>
2022-10-13 17:33:50 +0300

    update file

--- 02-Unit-Tests/auth-app/server/config/index.js
+++ 02-Unit-Tests/auth-app/server/config/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 
-dotenv.config()
+dotenv.config({ path: '.env' })
 
 export default {
     url: process.env.APP_URL || 'http://localhost:3001',
@@ -10,13 +10,13 @@ export default {
     databaseUrl: {
         development:
             process.env.DEVELOPMENT_DATABASE_URL ||
-            'mongodb://localhost:27017/mernmongo',
+            'mongodb://localhost:27017/auth-app',
         production:
             process.env.PRODUCTION_DATABASE_URL ||
             'mongodb://localhost:27017/mernmongo_production',
         test:
             process.env.TEST_DATABASE_URL ||
-            'mongodb://localhost:27017/mernmongo_test'
+            'mongodb://localhost:27017/auth-app_test'
     },
 
     jwtSecret: process.env.JWT_SECRET || '1234',
